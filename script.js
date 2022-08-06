6dcb1ebe5dd7d0b94ede412942829298dbefe781
Dominik Blak <bldominikak@gmail.com>
2022-08-06 15:45:40 +0200

    create array and new values

---
+++ script.js
@@ -0,0 +1,7 @@
+const randomArray = [];
+const clickArray = [];
+const numberOfSteps = document.querySelector(".js-startGame");
+const buttonReset = document.querySelector(".js-reset");
+const buttonStartGame = document.querySelector(".js-startGame");
+
+const newGame = () => {};
