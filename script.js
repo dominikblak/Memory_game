d28c4eb1e0f0ed8a688a3f17e391beafcc2ded76
Dominik Blak <bldominikak@gmail.com>
2022-08-06 18:27:24 +0200

    buttonStartGame addEventListener

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const randomArray = [];
 const clickArray = [];
-const numberOfSteps = document.querySelector(".js-startGame");
+const numberOfSteps = document.querySelector(".js-steps");
 const buttonReset = document.querySelector(".js-reset");
 const buttonStartGame = document.querySelector(".js-startGame");
 
-const newGame = () => {};
+buttonStartGame.addEventListener("click", () => {
+  const choiceNumberOfSteps = document.getElementById("steps").value;
+  console.log(choiceNumberOfSteps);
+});
+
+// const numberOfSteps = document.querySelector(".js-steps");
+// const choiceNumberOfSteps = numberOfSteps.value;
+// for (let steps = choiceNumberOfSteps; i <= steps; steps++) {
+//   rand = Math.floor(Math.random() * 9) + 1;
+//   randomArray.push(rand);
+// }
