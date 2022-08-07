e12e2a177e574625ac23ff73b029547281cf39b7
Dominik Blak <bldominikak@gmail.com>
2022-08-07 11:50:43 +0200

    Tiles shown have been added

--- script.js
+++ script.js
@@ -5,13 +5,29 @@ const buttonReset = document.querySelector(".js-reset");
 const buttonStartGame = document.querySelector(".js-startGame");
 
 buttonStartGame.addEventListener("click", () => {
-  startGame();
+  create_game();
 });
-const startGame = () => {
+const create_game = () => {
   const choiceNumberOfSteps = document.getElementById("steps").value;
   for (i = 1; i <= choiceNumberOfSteps; i++) {
     draw = Math.floor(Math.random() * 17) + 1;
     randomArray.push(draw);
   }
+  output_signals();
   console.log(randomArray);
+};
+const animate_sequence_button = (id) => {
+  const button = document.getElementById(id);
+  button.classList.add("active");
+  setTimeout(() => {
+    button.classList.remove("activeLeftTitle");
+  }, 400);
+};
+
+const output_signals = (id) => {
+  randomArray.forEach((title, index) => {
+    setTimeout(() => {
+      animate_sequence_button("tileleft" + title);
+    }, 1000);
+  });
 };
